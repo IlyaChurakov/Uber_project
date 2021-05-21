acfd4fc3d05b1a11a943b47e0f25b804d66a533c
Ilya <churakov018@mail.ru>
2021-05-21 21:47:59 +0300

    Final

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach((e) => {
+    e.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+    });
+});
